7b63f47c95ac7883adf302e3131096d3f1295387
Syndia <devteam@syndia.nl>
2017-04-28 10:46:20 +0200

    Changes: Footer small

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,12 +6,27 @@ const Footer = () => (
   <footer className={ styles.footer }>
     { /* If you like Phenomic, this is a way to share the love ;) */ }
     <p>
+      { "Website by "}
+      <a
+        href="https://www.syndia.nl"
+        className={ styles.footerReferenceName }
+        target="_blank"
+        rel="nooper noreferrer"
+      >
+        <span className={ styles.footerReferenceName }>
+          { "Syndia's Web Studio" }
+        </span>
+      </a>
+      { ". Made with " }
+      <span className={ styles.heart }>{ "♥" }</span>
+      { " and " }
       <a
         href={ process.env.PHENOMIC_HOMEPAGE }
-        className={ styles.phenomicReference }
+        className={ styles.footerReference }
+        target="_blank"
+        rel="nooper noreferrer"
       >
-        { "Website generated with " }
-        <span className={ styles.phenomicReferenceName }>
+        <span className={ styles.footerReferenceName }>
           {  `<${ process.env.PHENOMIC_NAME} />` }
         </span>
       </a>
